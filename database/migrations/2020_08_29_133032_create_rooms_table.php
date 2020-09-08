bc233f869ef0128fd2faa0470c8c149631067206
Fabio Dukievicz <fabiojd47@gmail.com>
2020-09-07 23:59:33 -0300

    migrations

---
+++ database/migrations/2020_08_29_133032_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('hotel_id');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->string('detail')->nullable();
+            $table->integer('category')->nullable();
+            $table->timestamps();
+            $table->foreign('hotel_id')->references('id')->on('hotels')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
