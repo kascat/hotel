dfbbdb364957edfef03b9704ffd009e19e89bcbc
Fabio Dukievicz <fabiojd47@gmail.com>
2020-09-08 00:05:41 -0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -7,3 +10,23 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/reservation', 'ReservationController@index')->name('reservation');
+Route::post('/new_reservation', 'ReservationController@new')->name('new_reservation');
+
+Route::resources([
+    'hotels' => 'HotelController',
+    'rooms' => 'RoomController'
+]);
+
+//  ____________________________________________________________________
+// | Verb         | URI                    | Action    | Route Name     |
+// |--------------|------------------------|-----------|----------------|
+// | GET          | /photos                | index     | photos.index   |
+// | GET          | /photos/create         | create    | photos.create  |
+// | POST         | /photos                | store     | photos.store   |
+// | GET          | /photos/{photo}        | show      | photos.show    |
+// | GET          | /photos/{photo}/edit   | edit      | photos.edit    |
+// | PUT/PATCH    | /photos/{photo}        | update    | photos.update  |
+// | DELETE       | /photos/{photo}        | destroy   | photos.destroy |
+// '--------------------------------------------------------------------'
